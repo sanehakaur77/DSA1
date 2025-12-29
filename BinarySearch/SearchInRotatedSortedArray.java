36bd8b8c6ca1b5c2e81bf0f0b788541e4bfd0331
sanehakaur77 <snehaswali9988@gmail.com>
2025-12-29 13:33:41 +0530

    33

---
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            // left half sorted
+            if(nums[low]<=nums[mid]){
+              //  check if target lies in the left sorted part
+              if(nums[low]<=target && target<nums[mid]){
+               high=mid-1;
+              }
+              else{
+                low=mid+1;
+              }  
+            }
+            //  Right half is sorted
+            else{
+              //  check if the target lies in the right half sorted part
+              if(nums[mid]<target && target<=nums[high]){
+                low=mid+1;
+              }
+              else{
+                high=mid-1;
+              }
+            }
+        }
+        return -1;
+    }
+}
